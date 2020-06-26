25c0ccce3379fd3f184f89850d713c2c9a0a7afd
rvishwan <rakeshvishwanath@gmail.com>
2020-06-26 07:58:55 -0700

    changed folder for deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       key: '/home/rakesh/rak/covid19.pem',
       ref: 'origin/master',
       repo: 'git@github.com:rvishwan/PGPCCCoronaAnalysisPjct.git',
-      path: '/home/ubuntu/gitrepofolder',
+      path: '/home/ubuntu/covid19projectserver',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
