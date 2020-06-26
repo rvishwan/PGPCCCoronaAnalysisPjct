50785927e956ceda6a0bb459ecc7ad3b3cef3b30
rvishwan <rakeshvishwanath@gmail.com>
2020-06-26 07:39:59 -0700

    Commit for main web server on ec2 instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-87-196-93.compute-1.amazonaws.com',
-      key: '/home/rakesh/rak/coronaKeypair.pem',
+      host: 'ec2-100-25-191-1.compute-1.amazonaws.com',
+      key: '/home/rakesh/rak/covid19.pem',
       ref: 'origin/master',
       repo: 'git@github.com:rvishwan/PGPCCCoronaAnalysisPjct.git',
       path: '/home/ubuntu/gitrepofolder',
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
+
+const port =3000
+// app.get('/', (req, res) => {
+//   res.send('HEY!')
+// })
+
+app.use(express.static('dist/spa'))
+
+app.listen(port, () => console.log('Server running on port: )!', port))
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(function(req, res) {
+  var payload = req.body
+  accessUserStatus(req, res)
+  console.log(payload)
+ // var test = payload['message']
+  //console.log(test)
+  //res.send(req.body)
 })
 
-app.listen(3000, () => console.log('Server running on port 3000'))
-
+const accessUserStatus = (request, response) => {
+  const { location } = request.body
+  console.log(location)
+  response.status(201).send(`Submitted `)
+}
