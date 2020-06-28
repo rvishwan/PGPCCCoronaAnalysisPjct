6825808a1e62114ef9bba4fce04bdc373530bc99
Ashwin Krishnamurthi <kashwin92@gmail.com>
2020-06-28 19:07:55 +0530

    updated index.js

--- index.js
+++ index.js
@@ -20,16 +20,40 @@ app.use('/analysestatus',(req, res) => {
   accessUserStatus(req, res)
 })
 
+var AWS = require("aws-sdk")
+	AWS.config.update({
+			"region": "us-east-1",
+			"endpoint": "http://dynamodb.us-east-1.amazonaws.com"
+	});
+
 const accessUserStatus = (request, response) => {
   var location  = request.body
   console.log(location.city)
   console.log(location.state)
 
   //TO DO :This is sample checking, needs to be replaced once we read from db
-  if(location.city == 'Bengaluru'){
+/*  if(location.city == 'Bengaluru'){
       response.status(201).send('Safe')
   }
   else{
     response.status(201).send('Unsafe')
-  }
+  }*/
+  const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+
+	var params = {
+		TableName: "covid19citydata",
+		Key: {
+			"state": location.state,
+			"city": location.city
+		}
+	};
+	
+	docClient.get(params, function(err, data) {
+		if (err) {
+			console.log("covid19citydata::fetchOneByKey::error -  " + JSON.stringify(err, null, 2));
+		}
+		else {
+			console.log("covid19citydata::fetchOneByKey::success -  " + JSON.stringify(data, null, 2));
+		}
+	});
 }
