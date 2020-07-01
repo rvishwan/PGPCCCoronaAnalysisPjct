12b849738bb64dd8b1d779c6afab941e8e13a9af
rvishwan <rakeshvishwanath@gmail.com>
2020-07-01 01:37:47 -0700

    added 2nd config for 2nd ec2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,15 @@ module.exports = {
       repo: 'git@github.com:rvishwan/PGPCCCoronaAnalysisPjct.git',
       path: '/home/ubuntu/covid19projectserver',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    },
+    production2: {
+      user: 'ubuntu',
+      host: 'ec2-3-236-102-91.compute-1.amazonaws.com',
+      key: '/home/rakesh/rak/covid19.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:rvishwan/PGPCCCoronaAnalysisPjct.git',
+      path: '/home/ubuntu/covid19projectserver',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }
